dbe72b0454623afae7cf212f6f5d3d3db1b353d3
hyuksoo sin <sins051301@gamil.com>
2024-03-19 19:46:00 +0900

    jsversion

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import MapPage from 'pages/MapPage';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -9,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <MapPage />
   </React.StrictMode>,
 );
 
