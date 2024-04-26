28c86cd5c95622210a31f24bf9a3375b95d64098
hyuksoo sin <sins051301@gamil.com>
2024-04-26 17:25:05 +0900

    사이드 초기 개발 버전

--- src/components/map/KaKaoHeader.tsx
+++ src/components/map/KaKaoHeader.tsx
@@ -2,8 +2,9 @@ import { Dispatch } from 'react';
 import styles from './KaKaoHeader.module.scss';
 import DeleteMarks from './DeleteMarks';
 import markers, { DataMarkerProps } from './data';
-
+// 카테고리 헤더
 function KaKaoHeader({ setCategory }: any): JSX.Element {
+  // type에 any를 써준 모습.. 리덕스로 수정예정
   return (
     <div className={styles.category}>
       <img src="../logo192.png" alt="LogoImage" className={styles.ImgStyle} />
